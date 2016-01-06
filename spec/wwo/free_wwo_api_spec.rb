f5c3cfad608970cdc9d04897cafa61025b56423b
Ryan Stenhouse <ryan@ryanstenhouse.eu>
2016-01-06 13:59:05 +0900

    More tests

--- spec/wwo/free_wwo_api_spec.rb
+++ spec/wwo/free_wwo_api_spec.rb
@@ -14,38 +14,70 @@ context "The WWO Online Free API" do
   context "Forecast.io Backwards Compatibility" do
 
     before :each do
-       Wwo.api_key = "your-api-key"
+       Wwo.api_key = "an-api-key"
      end
 
     it "exposes a forecast method" do
       expect(Wwo.respond_to?(:forecast)).to eq(true)
     end
 
-    it "returns a non-empty response for a valid request in the past" do
-      time = Time.utc(2016,1,1,0).to_i
-      expect(Wwo.api_key).to_not be_nil
+    context "for historic api requests" do
+      it "returns a non-empty response" do
+        time = Time.utc(2016,1,1,0).to_i
+        expect(Wwo.api_key).to_not be_nil
 
-      VCR.use_cassette("forecast_for_tokyo_on_jan_1st_2016") do
-        weather = Wwo.forecast(latitude, longitude, time: time)
-        expect(weather.inspect).to_not be_empty
+        VCR.use_cassette("forecast_for_tokyo_on_jan_1st_2016") do
+          weather = Wwo.forecast(latitude, longitude, time: time)
+          expect(weather.inspect).to_not be_empty
+        end
+      end
+
+      it "returns a response in the expected format" do
+        time = Time.utc(2016,1,1,0).to_i
+        expect(Wwo.api_key).to_not be_nil
+
+        VCR.use_cassette("forecast_for_tokyo_on_jan_1st_2016") do
+          weather = Wwo.forecast(latitude, longitude, time: time)
+
+          expect(weather.daily.data.size).to eq(1)
+          expect(weather.alerts).to be_nil
+
+          daily = weather.daily.data[0]
+
+          expect(daily.icon).to eq("http://cdn.worldweatheronline.net/images/wsymbols01_png_64/wsymbol_0001_sunny.png")
+          expect(daily.temperatureMax).to eq("15")
+          expect(daily.temperatureMin).to eq("4")
+        end
       end
     end
 
-    it "returns a response in the expected format for a request in the past" do
-      time = Time.utc(2016,1,1,0).to_i
-      expect(Wwo.api_key).to_not be_nil
+    context "for recent date api requests" do
+      it "returns a non-empty response" do
+        time = Time.utc(2016,1,10,0).to_i
+        expect(Wwo.api_key).to_not be_nil
 
-      VCR.use_cassette("forecast_for_tokyo_on_jan_1st_2016") do
-        weather = Wwo.forecast(latitude, longitude, time: time)
+        VCR.use_cassette("forecast_for_tokyo_on_jan_10th_2016") do
+          weather = Wwo.forecast(latitude, longitude, time: time)
+          expect(weather.inspect).to_not be_empty
+        end
+      end
 
-        expect(weather.daily.data.size).to eq(1)
-        expect(weather.alerts).to be_nil
+      it "returns a response in the expected format" do
+        time = Time.utc(2016,1,10,0).to_i
+        expect(Wwo.api_key).to_not be_nil
 
-        daily = weather.daily.data[0]
+        VCR.use_cassette("forecast_for_tokyo_on_jan_10th_2016") do
+          weather = Wwo.forecast(latitude, longitude, time: time)
 
-        expect(daily.icon).to eq("http://cdn.worldweatheronline.net/images/wsymbols01_png_64/wsymbol_0001_sunny.png")
-        expect(daily.temperatureMax).to eq("15")
-        expect(daily.temperatureMin).to eq("4")
+          expect(weather.daily.data.size).to eq(1)
+          expect(weather.alerts).to be_nil
+
+          daily = weather.daily.data[0]
+
+          expect(daily.icon).to eq("http://cdn.worldweatheronline.net/images/wsymbols01_png_64/wsymbol_0001_sunny.png")
+          expect(daily.temperatureMax).to eq("11")
+          expect(daily.temperatureMin).to eq("4")
+        end
       end
     end
 
