fff7f4b1161f2969d0fe16bcbd39d5f276e8bb8d
Ryan Stenhouse <ryan@ryanstenhouse.eu>
2016-03-01 17:16:29 +0900

    Handle date ranges better

--- lib/wwo.rb
+++ lib/wwo.rb
@@ -18,16 +18,54 @@ module Wwo
     # specified location.
     #
     def date_range(start_date, end_date, latitude, longitude, forecast_compat = false)
-      starting  = start_date.strftime('%F')
-      ending    = end_date.strftime('%F')
+      start_date_string = start_date.strftime('%F')
+      end_date_string   = end_date.strftime('%F')
+      today_date_string = Time.now.strftime('%F')
 
-      uri = "#{Wwo.api_endpoint}/past-weather.ashx?q=#{latitude},#{longitude}&format=json&extra=utcDateTime&date=#{starting}&enddate=#{ending}&show_comments=no&tp=24&key=#{Wwo.api_key}&mca=false&show_comments=false"
+      if start_date_string == today_date_string || range_in_future?(start_date, end_date)
+        uri = "#{Wwo.api_endpoint}/weather.ashx?q=#{latitude},#{longitude}&format=json&num_of_days=7&date=today&cc=no&mca=no&tp=24&key=#{Wwo.api_key}"
+        response = Hashie::Mash.new(api_call(uri))
+        if forecast_compat
+          return make_into_forecast_response(response)
+        else
+          return response
+        end
 
-      if forecast_compat
-        make_into_forecast_response(api_call(uri))
+      elsif starts_in_past_but_ends_in_future?(start_date, end_date)
+        yesterday_date_string = (Time.now - (24 *(60 * 60))).strftime('%F')
+
+        uri = "#{Wwo.api_endpoint}/past-weather.ashx?q=#{latitude},#{longitude}&format=json&extra=utcDateTime&date=#{start_date_string}&enddate=#{yesterday_date_string}&show_comments=no&tp=24&key=#{Wwo.api_key}&mca=false&show_comments=false"
+        past_response = Hashie::Mash.new(api_call(uri))
+        uri = "#{Wwo.api_endpoint}/weather.ashx?q=#{latitude},#{longitude}&format=json&num_of_days=7&date=today&cc=no&mca=no&tp=24&key=#{Wwo.api_key}"
+        future_response = Hashie::Mash.new(api_call(uri))
+
+        if forecast_compat
+          past    = make_into_forecast_response(past_response)
+          future  = make_into_forecast_response(future_response)
+          past[:daily][:data] = (past[:daily][:data] + future[:daily][:data]).flatten.uniq
+
+          return past
+        else
+          return past_response.deep_merge(future_response)
+        end
+
       else
-        api_call(uri)
+        uri = "#{Wwo.api_endpoint}/past-weather.ashx?q=#{latitude},#{longitude}&format=json&extra=utcDateTime&date=#{start_date_string}&enddate=#{end_date_string}&show_comments=no&tp=24&key=#{Wwo.api_key}&mca=false&show_comments=false"
+        response = Hashie::Mash.new(api_call(uri))
+        if forecast_compat
+          return make_into_forecast_response(response)
+        else
+          return response
+        end
       end
+    end
+
+    def starts_in_past_but_ends_in_future?(start_date, end_date)
+      start_date.to_i < Time.now.to_i && end_date.to_i >= Time.now.to_i
+    end
+
+    def range_in_future?(start_date, end_date)
+      end_date.to_i >= Time.now.to_i && start_date.to_i >= Time.now.to_i
     end
 
     # Returns an hourly breakdown for the weather "today" at the given location. We get
@@ -117,7 +155,9 @@ module Wwo
     # Munges the repsonse into one like what we would expect from Forecast.io
     #
     def make_into_forecast_response(response)
-      if ! response.data.weather.nil? && response.data.weather.any? && response.data.weather.size > 1
+      if response.is_a?(Hash) && response.empty?
+        return { daily: { data: [] } }
+      elsif ! response.data.weather.nil? && response.data.weather.any? && response.data.weather.size > 1
         data = { daily: { data: [] } }
         response.data.weather.each do |weather|
           icon = weather.hourly.first.weatherIconUrl.first.value
@@ -127,13 +167,14 @@ module Wwo
 
           data[:daily][:data] << { icon: icon, "temperatureMax" => maxTemp, "temperatureMin" => minTemp, date: date }
         end
+        return data
       else
         data = { daily: { data: [ { icon: '', 'temperatureMax' => 0, 'temperatureMin' => 0  } ] }, alerts: nil }
         data[:daily][:data][0][:icon] = response.data.weather.first.hourly.first.weatherIconUrl.first.value
         data[:daily][:data][0]['temperatureMax'] = response.data.weather.first.maxtempC
         data[:daily][:data][0]['temperatureMin'] = response.data.weather.first.mintempC
+        return data
       end
-      Hashie::Mash.new(data)
     end
   end
 end
